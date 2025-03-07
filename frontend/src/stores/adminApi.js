b5d3f7d63b24302fdfded09341c0f4c8ed62c22e
JohnLloydDev <johnlloyd.romero1002@outlook.com>
2025-03-08 07:56:54 +0800

    dd

--- frontend/src/stores/adminApi.js
+++ frontend/src/stores/adminApi.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { create } from "zustand";
 import toast from "react-hot-toast";
 
-const BASE_URL = "http://localhost:8080/api/admin";
+const BASE_URL = "https://deploy-disability-career-git-main-john-lloyds-projects-a10e9887.vercel.app/api/admin";
 
 axios.defaults.withCredentials = true;
 export const adminStore = create((set) => ({
